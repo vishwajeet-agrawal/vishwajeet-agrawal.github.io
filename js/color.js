30fc50b2ab8f6de5164c28954760234fd907874c
VISHWAJEET AGRAWAL <43827493+vishwajeet-agrawal@users.noreply.github.com>
2021-07-18 16:13:30 +0530

    Update color.js

--- js/color.js
+++ js/color.js
@@ -1,58 +1,99 @@
 var correct = 0;
 var total = 0;
 const colors = ["rgb(170,0,0)", "rgb(0,170,0)", "rgb(0,0,170)"];
-const colors1 = ["rgb(100,0,0)", "rgb(0,100,0)", "rgb(0,0,100)"];
+const colors1 = ["rgb(80,0,0)", "rgb(0,80,0)", "rgb(0,0,80)"];
+const colors2 = ["rgb(160,0,0)", "rgb(0,160,0)", "rgb(0,0,160)"];
+
 var ci = -1;
 var started = 0;
+var iskey = false;
+var isclick = true;
+const white = "rgb(255,255,255)"
+const startb = document.getElementById("start");
+const keyinfo = document.getElementById("keyinfo");
+const clickinfo = document.getElementById("clickinfo");
+const result = document.getElementById("result")
+const showb = document.getElementById("show");
+
+showb.style.display = "none";
+keyinfo.style.display = "none";
+
 function nextColor() {
     ci = Math.floor(Math.random()*3);
-    document.body.style.backgroundColor = colors1[ci];
+    document.body.style.backgroundColor = colors[ci];
+    // setTimeout(function(){
+    //     document.body.style.backgroundColor = colors[ci];
+    // }, 500)
+}
+function nextColorClick() {
+    ci = Math.floor(Math.random()*3);
+    document.body.style.backgroundColor = colors2[ci];
     setTimeout(function(){
         document.body.style.backgroundColor = colors[ci];
-    }, 500)
+    }, 200)
 }
 document.querySelector('body').onclick = function(){
-    if (started==1){
-        nextColor();
+    if (isclick){
+        nextColorClick();
     }
 }
 document.querySelector('body').onkeypress = function(event){
-    if (started == 1){
-    if (event.key == '1'){
-        if (ci==0){
-            correct += 1;
+    if (iskey){
+        if (event.key == ' '){
+            started = 1;
+            correct = 0;
+            total = 0;
+            result.textContent = "";
+            showb.style.display = "block";
+            keyinfo.style.display = "none";  
+               
         }
-        total += 1;
-    }
-    else if (event.key=='2'){
-        if (ci==1){
-            correct += 1;
+        if (started == 1){
+            if (event.key == '1'){
+                if (ci==0){
+                    correct += 1;
+                }
+                total += 1;
+                document.body.style.backgroundColor = colors1[0]; 
+            }
+            else if (event.key=='2'){
+                if (ci==1){
+                    correct += 1;
+                }
+                total += 1;
+                document.body.style.backgroundColor = colors1[1]; 
+            }
+            else if (event.key=='3'){
+                if (ci==2){
+                    correct += 1;
+                }
+                total += 1;
+                document.body.style.backgroundColor = colors1[2]; 
+            }
+            setTimeout(nextColor, 500);
         }
-        total += 1;
     }
-    else if (event.key=='3'){
-        if (ci==2){
-            correct += 1;
-        }
-        total += 1;
-    }
-    nextColor();
 }
+
+
+function show(){
+    started = 0;
+    showb.style.display = "none";
+    result.textContent = "Correct: "+ correct.toString() + ", Total: " + total.toString() + ", Accuracy: " + (correct/total).toFixed(2).toString();
+    startb.style.display = "block";
+    iskey = false;
+    isclick = true;
 }
-const button = document.getElementById("button")
-button.onclick = function(){
-    if (started == 0){
-        button.textContent = "End";
-        started = 1;
-    }
-    else if (started == 1){
-        button.textContent = "Correct: "+ correct.toString() + ", Total: " + total.toString() + ", Accuracy: " + (correct/total+1e-6).toFixed(2).toString();
-        started = 2;
-    }
-    else if (started == 2){
-        started = 0;
-        button.textContent = "Start";
-        correct = 0;
-        total = 0;
-    }
+
+function startkey(){
+    iskey = true;
+    isclick = false;
+    correct = 0;
+    total = 0;
+    document.body.style.backgroundColor = white;
+    startb.style.display = "none";
+    showb.style.display = "none";
+    keyinfo.style.display = "block";
+    clickinfo.style.display = "none";
+    result.textContent = "";
 }
